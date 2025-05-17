ece01dccc89d6ac12e15875b724e8e074dde19e2
Ican <sumiarti130568@gmail.com>
2025-05-17 12:45:03 +0700

    PUSH

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,12 @@
-
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: '#f472b6',
-        secondary: '#fce7f3',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
